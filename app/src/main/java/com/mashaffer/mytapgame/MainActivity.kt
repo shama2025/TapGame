66bb1ce73c309965d89cf1880898d39a50f1a860
shama2025 <2003mashaffer@gmail.com>
2025-02-05 14:56:00 -0500

    *Update activity_main.xml *Update MainActivity.kt *Update strings.xml

--- app/src/main/java/com/mashaffer/mytapgame/MainActivity.kt
+++ app/src/main/java/com/mashaffer/mytapgame/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.mashaffer.mytapgame
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import org.w3c.dom.Text
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val TAG = "MainActivity";
+    }
+
     // Activity Level Variables
     private lateinit var tapBtn: ImageButton; // Image button for tapping
     private lateinit var numTaps: TextView; // Records the number of taps
     private lateinit var daysLoggedIn: TextView; // Records the days logged into the app (not idle)
+    private lateinit var main_nav: BottomNavigationView // App Navigation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         tapBtn = findViewById(R.id.imgBtn);
         numTaps = findViewById(R.id.numTaps);
-
+        main_nav = findViewById(R.id.main_navigation);
         setup();
     }
 
@@ -32,5 +39,23 @@ class MainActivity : AppCompatActivity() {
             taps++;
             numTaps.text = "Taps: ${taps}";
         })
+
+        main_nav.setOnItemReselectedListener{ item ->
+            when (item.itemId) {
+                R.id.home_page_btn -> {
+                    Log.i("BottomNav", "You clicked Home")
+                    true
+                }
+                R.id.leaderboard_btn -> {
+                    Log.i("BottomNav", "You clicked Leaderboard")
+                    true
+                }
+                R.id.status_btn -> {
+                    Log.i("BottomNav", "You clicked Status")
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
