40afd36d0a1eea2378c5434d9300efcc122ffa31
shama2025 <2003mashaffer@gmail.com>
2025-02-09 20:06:29 -0500

    *Update Main Activity *Create Integers.xml *Update strings.xml *Update activity_main.xml

--- app/src/main/java/com/mashaffer/mytapgame/MainActivity.kt
+++ app/src/main/java/com/mashaffer/mytapgame/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.mashaffer.mytapgame
 
+
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -19,9 +19,12 @@ class MainActivity : AppCompatActivity() {
     // Activity Level Variables
     private lateinit var tapBtn: ImageButton; // Image button for tapping
     private lateinit var numTaps: TextView; // Records the number of taps
-    private lateinit var daysLoggedIn: TextView; // Records the days logged into the app (not idle)
-    private lateinit var main_nav: BottomNavigationView // App Navigation
+    private lateinit var highScore: TextView; // Records the days logged into the app (not idle)
+    private lateinit var mainNav: BottomNavigationView // App Navigation
 
+    // Figure out how to implement saving game data when app closes
+    private var savedHighScore:Int = 0
+    private val activity = this
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,18 +32,28 @@ class MainActivity : AppCompatActivity() {
 
         tapBtn = findViewById(R.id.imgBtn);
         numTaps = findViewById(R.id.numTaps);
-        main_nav = findViewById(R.id.main_navigation);
+        mainNav = findViewById(R.id.main_navigation);
+        highScore = findViewById(R.id.high_score);
         setup();
     }
 
     private fun setup(){
-        var taps: Int = 0;
+        var highScoreTaps: Int = getHighScore()
+        var taps: Int = getLastTaps();
+        highScore.text = "High Score: ${highScoreTaps}"
+
+
+
         tapBtn.setOnClickListener({
             taps++;
+            if(taps >= highScoreTaps){
+                highScore.text = "High Score: ${taps}"
+                writeToHighScore(taps)
+            }
             numTaps.text = "Taps: ${taps}";
         })
 
-        main_nav.setOnItemReselectedListener{ item ->
+        mainNav.setOnItemReselectedListener{ item ->
             when (item.itemId) {
                 R.id.home_page_btn -> {
                     Log.i("BottomNav", "You clicked Home")
@@ -48,14 +61,49 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.leaderboard_btn -> {
                     Log.i("BottomNav", "You clicked Leaderboard")
+                    writeCurrentTaps(taps)
                     true
                 }
-                R.id.status_btn -> {
-                    Log.i("BottomNav", "You clicked Status")
-                    true
-                }
+//                R.id.status_btn -> {
+//                    Log.i("BottomNav", "You clicked Status")
+//                    true
+//                }
                 else -> false
             }
         }
     }
+
+    private fun writeCurrentTaps(currentTaps: Int) {
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            putInt(getString(R.string.curernt_taps), currentTaps)
+            apply()
+        }
+    }
+
+    private fun writeToHighScore(newHighScore:Int) {
+        // Will write to the strings.xml file using the shared pref
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            putInt(getString(R.string.current_high_score_key), newHighScore)
+            apply()
+        }
+    }
+
+    private fun getHighScore(): Int{
+        // Using the shared prefs library, will get the current highscore that is stored
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return 0
+        val defaultValue = resources.getInteger(R.integer.saved_high_score_val)
+        val highScore = sharedPref.getInt(getString(R.string.current_high_score_key), defaultValue)
+        return highScore
+    }
+
+    private fun getLastTaps(): Int{
+        // Using the shared prefs library, will get the current highscore that is stored
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return 0
+        val defaultValue = resources.getInteger(R.integer.saved_last_taps)
+        val lastTaps = sharedPref.getInt(getString(R.string.current_high_score_key), defaultValue)
+        return lastTaps
+    }
+
 }
